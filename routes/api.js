b495ab73e556c4b2383a0b9c79b8895d5ed62c33
julian <juliandecastelli@gmail.com>
2022-08-17 19:37:14 -0300

    add mongodb and mogoose dependencies

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,41 @@
-'use strict';
+"use strict";
 
 module.exports = function (app) {
+  const mongoose = require("mongoose");
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const projectShema = new mongoose.Schema({
+    assigned_to: String,
+    status_text: String,
+    open: Boolean,
+    issue_title: String,
+    issue_text: String,
+    created_by: String,
+    created_on: Date,
+    updated_on: Date,
+  });
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+  let Project = mongoose.model("Project", projectShema);
+
+  app
+    .route("/api/issues/:project")
+
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
+      console.log(req.body.issue_title);
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
